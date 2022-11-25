badb3a4cc683c9aa142ebd5ba3e907ee8e2c958f
Juan Jose Rodriguez <juanjose@euler.tools>
2022-11-25 12:00:29 +0000

    test job

--- models/public_data/data2.sql
+++ models/public_data/data2.sql
@@ -4,7 +4,7 @@
         unique_key=['date','user_id','user_uid']
     )
 }}
-with source_data as (select * from fns.gethubspotmetrics('zinkee', now()::DATE))
+with source_data as (select * from fns.gethubspotmetrics('zinkee', date('2022-11-23')::DATE))
 
 select *
 from source_data
